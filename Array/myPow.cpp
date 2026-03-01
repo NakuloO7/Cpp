cbcc377ca7e149b729ae74193cd0e366125cf702
Nakul <nakulgite08@gmail.com>
2026-03-01 13:48:22 +0530

    recursive soln

---
+++ Array/myPow.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <climits>
+#include <cmath>
+using namespace std;
+
+int pow(int x, int n){
+    if(n == 0) return 1;
+
+    return x * pow(x, n-1);
+}
+int main(){
+    cout<<pow(2, 4);
+ return 0;
+}
