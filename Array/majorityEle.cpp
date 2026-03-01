b2b5ea97324b0360d3d906ec5457bceefb85d45c
Nakul <nakulgite08@gmail.com>
2026-03-01 17:21:08 +0530

    majority 2 optimal

--- Array/majorityEle.cpp
+++ Array/majorityEle.cpp
@@ -12,39 +12,60 @@
 #include <cmath>
 using namespace std;
 
-int majorityEle(vector<int>arr){
+vector<int> majorityEle(vector<int>arr){
     int n = arr.size();
-    int count = 0;
-    int ele = 0;
+    int cnt1 = 0;
+    int cnt2 = 0;
+    int ele1 = 0;
+    int ele2 = 0;
 
-    for(int i = 0; i<n; i++){
-        if(count == 0){
-            ele = arr[i];
-            count++;
-        }else if(arr[i] == ele){
-            count++;
+    for(int i=0; i<n; i++){
+        if(cnt1 == 0 && ele2 != arr[i]){
+            cnt1 = 1;
+            ele1 = arr[i];
+        }else if(cnt2 == 0 && ele1 != arr[i]){
+            cnt2 = 1;
+            ele2 = arr[i];
+        }else if(ele1 == arr[i]){
+            cnt1++;
+        }else if(ele2 == arr[i]){
+            cnt2++;
         }else{
-            count--;
+            cnt1--;
+            cnt2--;
         }
     }
 
-    int target=0;
+    cnt1 = 0;
+    cnt2 = 0;
+
+    int freq = n/3 + 1;
     for(int i = 0; i<n; i++){
-        if(arr[i] == ele){
-            target++;
+        if(arr[i]== ele1){
+            cnt1++;
+        }else if(arr[i] == ele2){
+            cnt2++;
         }
     }
+    vector<int>ans;
 
-    if(target > n/2){
-        return ele;
+    if(cnt1 >= freq){
+        ans.push_back(ele1);
     }
-    return -1;
+    if(cnt2 >= freq){
+        ans.push_back(ele2);
+    }
+    return ans;
 }
  
 int main(){
 
-    vector<int>arr = {2,2,1,1,1,2,2};
-    cout<<majorityEle(arr);
+    vector<int>arr = {1, 2, 1, 1, 3, 2, 2};
+    vector<int>ans = majorityEle(arr);
+
+    for(auto i : ans){
+        cout<<i<<" ";
+    }
  
  return 0;
 }
